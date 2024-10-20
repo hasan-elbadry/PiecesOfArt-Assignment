55cac284acfa5fa8ca3b28ee13767d2e14eb4bf8
hasan-elbadru <hasan.elbadry2006@gmail.com>
2024-10-20 14:55:48 +0300

    Add project files.

---
+++ PiecesOfArt Assignment/Models/PieceOfArt.cs
@@ -0,0 +1,10 @@
+﻿namespace PiecesOfArt_Assignment.Models
+{
+    public class PieceOfArt
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public DateTime PublicationDate { get; set; } = DateTime.Now;
+    }
+}
